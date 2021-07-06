31ee6cedfa6fde9523a178bd2a75f2071b227bd0
Hossey <ees-ballon@mail.ru>
2021-07-06 17:44:43 +0500

    проект Тех.Центр

---
+++ src/main/java/com/hossey/techCenter/controller/Registration.java
@@ -0,0 +1,14 @@
+package com.hossey.techCenter.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+public class Registration {
+   // @RequestMapping(value = "/registration", method = RequestMethod.GET)
+   // public {
+
+   // }
+}
---
+++ src/test/java/com/hossey/techCenter/TechCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hossey.techCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
